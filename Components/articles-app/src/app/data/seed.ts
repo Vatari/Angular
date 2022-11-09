fb8a0ae8cfa9b31bf112ed65f606357a2a0ce05a
vatari <majestic13@gmail.com>
2022-11-09 18:47:20 +0200

    Angular exercises

---
+++ Components/articles-app/src/app/data/seed.ts
@@ -0,0 +1,43 @@
+const data = [
+  {
+    title: 'Article 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
+    author: 'Author 1',
+    imageUrl:
+      'https://e7.pngegg.com/pngimages/502/373/png-clipart-selective-focus-graphy-body-of-water-light-ocean-underwater-deep-sea-beautiful-ocean-views-of-the-ocean-cloud-photography-thumbnail.png',
+  },
+
+  {
+    title: 'Article 2',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
+    author: 'Author 2',
+    imageUrl:
+      'https://e7.pngegg.com/pngimages/535/234/png-clipart-magic-secrets-of-a-witch-s-coven-incantation-witchcraft-classical-element-magic-book-love-miscellaneous-thumbnail.png',
+  },
+  {
+    title: 'Article 3',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
+    author: 'Author 3',
+    imageUrl:
+      'https://e7.pngegg.com/pngimages/973/954/png-clipart-male-person-and-blue-question-mark-illustration-question-mark-faq-export-male-blue-cartoon-character-question-mark-cartoon-character-blue-thumbnail.png',
+  },
+  {
+    title: 'Article 4',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
+    author: 'Author 4',
+    imageUrl:
+      'https://e7.pngegg.com/pngimages/33/920/png-clipart-elephant-winnie-the-pooh-lumpy-kaplan-tigger-piglet-elephant-purple-mammal-thumbnail.png',
+  },
+  {
+    title: 'Article 5',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
+    author: 'Author 5',
+    imageUrl:
+      'https://e7.pngegg.com/pngimages/458/826/png-clipart-grey-bird-and-withered-plant-illustration-sculpture-surrealism-artist-raven-png-material-branch-thumbnail.png',
+  },
+];
