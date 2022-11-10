592b12d019e3368ca023b84fd449345d98ac520b
vatari <majestic13@gmail.com>
2022-11-10 19:47:39 +0200

    added css

--- workshop-components/app/src/app/app.module.ts
+++ workshop-components/app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { CoreModule } from './core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  declarations: [AppComponent, ThemeListComponent, RecentPostsComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
