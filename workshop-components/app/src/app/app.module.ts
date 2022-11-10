3c536e939ccb008d77e1ce4a3bab464acc243682
vatari <majestic13@gmail.com>
2022-11-10 20:55:08 +0200

    added loader

--- workshop-components/app/src/app/app.module.ts
+++ workshop-components/app/src/app/app.module.ts
@@ -8,10 +8,22 @@ import { ThemeListComponent } from './theme-list/theme-list.component';
 import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, ThemeListComponent, RecentPostsComponent, MainComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ThemeListComponent,
+    RecentPostsComponent,
+    MainComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
