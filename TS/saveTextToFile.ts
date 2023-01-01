15a4c9251e779e9141ec7b7f9c2f964ebd5ad3f0
vatari <majestic13@gmail.com>
2023-01-01 10:50:43 +0200

    added ts exercise

---
+++ TS/saveTextToFile.ts
@@ -0,0 +1,13 @@
+
+const fs = require('fs');
+
+async function main() {
+  const text = prompt('Enter some text to save:');
+  await fs.promises.writeFile('text.txt', text);
+  console.log(`Saved "${text}" to text.txt`);
+
+  const savedText = await fs.promises.readFile('text.txt', 'utf-8');
+  console.log(`Read from text.txt: "${savedText}"`);
+}
+
+main();
