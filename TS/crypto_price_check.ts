9df522d7df3ca2339c37ff0c9c0ac6e866c631d2
vatari <majestic13@gmail.com>
2023-01-05 19:34:20 +0200

    crypto price check ts script

--- TS/crypto_price_check.ts
+++ TS/crypto_price_check.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from '../node_modules/axios';
 
 async function getCryptoPrice(crypto: string): Promise<number> {
   const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${crypto}&vs_currencies=usd`);
