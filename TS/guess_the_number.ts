6171594d5427be2550625213f5143e2ab4d3914b
vatari <majestic13@gmail.com>
2023-01-08 07:33:31 +0200

    simple console game

--- TS/guess_the_number.ts
+++ TS/guess_the_number.ts
@@ -1,12 +1,14 @@
+const process = require("process");
+
 // Generate a random number between 1 and 100
 const target = Math.floor(Math.random() * 100) + 1;
 
 let guess: number;
 
-process.stdin.setEncoding('utf8');
-process.stdout.write('Guess a number between 1 and 100: ');
+process.stdin.setEncoding("utf8");
+process.stdout.write("Guess a number between 1 and 100: ");
 
-process.stdin.on('readable', () => {
+process.stdin.on("readable", () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     // Convert the input to a number and store it in the `guess` variable
@@ -14,15 +16,15 @@ process.stdin.on('readable', () => {
 
     // Check if the player's guess is correct
     if (guess === target) {
-      console.log('You won!');
+      console.log("You won!");
       process.exit();
     } else if (guess > target) {
-      console.log('Your guess is too high');
+      console.log("Your guess is too high");
     } else {
-      console.log('Your guess is too low');
+      console.log("Your guess is too low");
     }
 
     // Prompt the player to enter another guess
-    process.stdout.write('Guess a number between 1 and 100: ');
+    process.stdout.write("Guess a number between 1 and 100: ");
   }
 });
